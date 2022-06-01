72293cd692cd9eb5f04c597c08cc6fc49287a771
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-01 11:36:23 +0200

    changed page title.. changed logo. logo styled.

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img alt="bc logo" src="./assets/icon.png" />
+  <HelloWorld msg="My First Page with Vuejs" />
 </template>
 
 <style>
@@ -17,5 +17,9 @@ import HelloWorld from './components/HelloWorld.vue'
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img {
+  width: 20rem;
+  height: 20rem;
 }
 </style>
